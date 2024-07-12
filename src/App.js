c5453875f38b533c12893c68788313c502bafe85
AliAkbar <masterakbaridev@gmail.com>
2024-07-12 19:24:37 +0330

    fourth commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="font-poppins">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
